d24d6c85925c6e7576f3c063ec063cdec823b1b1
Fredi Machado <fredisoft@gmail.com>
2016-04-25 16:36:07 -0300

    Started the raw data parser

---
+++ NetIRC.Tests/IRCMessageTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace NetIRC.Tests
+{
+    public class IRCMessageTests
+    {
+        [Fact]
+        public void CanSetRawData()
+        {
+            var rawData = "raw";
+            var ircMessage = new IRCMessage(rawData);
+
+            Assert.Equal(rawData, ircMessage.Raw);
+        }
+
+        [Fact]
+        public void CanParseThePrefix()
+        {
+            var prefix = "prefix";
+            var ircMessage = new IRCMessage($":{prefix} ");
+
+            Assert.Equal(prefix, ircMessage.Prefix);
+        }
+    }
+}
---
+++ NetIRC/IRCMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetIRC
+{
+    public class IRCMessage
+    {
+        public string Raw { get; }
+
+        private string prefix;
+
+        public string Prefix => prefix;
+
+        public IRCMessage(string rawData)
+        {
+            Raw = rawData;
+            Parse(rawData);
+        }
+
+        private void Parse(string rawData)
+        {
+            var indexOfNextSpace = 0;
+
+            if (RawDataHasPrefix)
+            {
+                indexOfNextSpace = rawData.IndexOf(' ');
+                prefix = rawData.Substring(1, indexOfNextSpace - 1);
+            }
+        }
+
+        public bool RawDataHasPrefix => Raw.StartsWith(":");
+    }
+}
