8a6b994cf22702fd9c6832e24863d866369c57c5
Fred Machado <fredisoft@gmail.com>
2017-05-11 10:30:09 +1000

    Converting CodeCoverage.cmd to a PowerShell script

---
+++ tests/CodeCoverage/CodeCoverage.ps1
@@ -0,0 +1,11 @@
+dotnet restore NetIRC.sln
+dotnet build NetIRC.sln --no-incremental -c Release /p:codecov=1
+
+nuget restore .\tests\CodeCoverage -PackagesDirectory .\tests\CodeCoverage
+
+.\tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"dotnet.exe" -targetargs:"test tests\NetIRC.Tests\NetIRC.Tests.csproj --no-build -c release /p:codecov=1" -hideskipped:All -oldStyle -output:.\NetIRC_coverage.xml
+
+$env:PATH = "C:\Python36;C:\Python36\Scripts\;$env:PATH"
+
+pip install codecov
+codecov -f "NetIRC_coverage.xml"
