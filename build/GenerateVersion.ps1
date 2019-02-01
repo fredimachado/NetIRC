e19d0eea06e7ed80bfa0b60e87031146ea3f81cf
Fredi Machado <fredisoft@gmail.com>
2019-02-01 16:41:35 +1100

    Write source branch

--- build/GenerateVersion.ps1
+++ build/GenerateVersion.ps1
@@ -1,5 +1,6 @@
 $commitHash = $env:BUILD_SOURCEVERSION.Substring(0, 7)
 
+Write-Host "Source branch: $env:BUILD_SOURCEBRANCH"
 Write-Host "Branch name: $env:BUILD_SOURCEBRANCHNAME"
 Write-Host "Commit hash: $commitHash"
 Write-Host "Build number: $env:BUILD_BUILDNUMBER"
