cd4659cf5057c36200b9d4a4ae203814c79a1097
Fredi Machado <fredisoft@gmail.com>
2019-02-01 16:47:45 +1100

    Show all env variables

--- build/GenerateVersion.ps1
+++ build/GenerateVersion.ps1
@@ -1,6 +1,8 @@
 $commitHash = $env:BUILD_SOURCEVERSION.Substring(0, 7)
 
-Write-Host "Source branch: $env:BUILD_SOURCEBRANCH"
 Write-Host "Branch name: $env:BUILD_SOURCEBRANCHNAME"
 Write-Host "Commit hash: $commitHash"
 Write-Host "Build number: $env:BUILD_BUILDNUMBER"
+
+Write-Host "Env variables:"
+Get-ChildItem Env:
