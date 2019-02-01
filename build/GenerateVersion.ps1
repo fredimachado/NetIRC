c873ddb8cdce4631e787de586f39642a61448de9
Fredi Machado <fredisoft@gmail.com>
2019-02-01 16:37:51 +1100

    Get first 7 characters from commit ID

--- build/GenerateVersion.ps1
+++ build/GenerateVersion.ps1
@@ -1,3 +1,5 @@
+$commitHash = $env:BUILD_SOURCEVERSION.Substring(0, 7)
+
 Write-Host "Branch name: $env:BUILD_SOURCEBRANCHNAME"
-Write-Host "Commit ID: $env:BUILD_SOURCEVERSION"
+Write-Host "Commit hash: $commitHash"
 Write-Host "Build number: $env:BUILD_BUILDNUMBER"
