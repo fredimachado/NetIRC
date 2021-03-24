a2711bf1a851ae94164f3830b52f2a6632a17c3e
Fredi Machado <fredisoft@gmail.com>
2021-03-25 01:06:39 +1100

    Add Client connection tests

---
+++ tests/NetIRC.Tests/ClientConnectionTests.cs
@@ -0,0 +1,57 @@
+﻿using NetIRC.Extensions;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetIRC.Tests
+{
+    public class ClientConnectionTests
+    {
+        private static User FakeUser = new User("test", "test");
+
+        [Fact]
+        public async Task WhenConnecting_ClientShouldSendRegistrationMessages()
+        {
+            var port = 6669;
+            var nickMessage = $"NICK {FakeUser.Nick}";
+            var userMessage = $"USER {FakeUser.Nick} 0 - {FakeUser.RealName}";
+
+            var tcpListener = new TcpListener(IPAddress.Loopback, port);
+            tcpListener.Start();
+
+            using var client = new Client(FakeUser);
+
+            client.ConnectAsync("localhost", port)
+                .SafeFireAndForget(continueOnCapturedContext: false);
+
+            using var server = await tcpListener.AcceptTcpClientAsync();
+            using var stream = new StreamReader(server.GetStream());
+
+            Assert.Equal(nickMessage, await stream.ReadLineAsync());
+            Assert.Equal(userMessage, await stream.ReadLineAsync());
+        }
+
+        [Fact]
+        public async Task WhenConnectingWithPassword_ClientShouldSendPassMessage()
+        {
+            var port = 6670;
+            var password = "passw0rd123";
+            var passMessage = $"PASS {password}";
+
+            var tcpListener = new TcpListener(IPAddress.Loopback, port);
+            tcpListener.Start();
+
+            using var client = new Client(FakeUser, password);
+
+            client.ConnectAsync("localhost", port)
+                .SafeFireAndForget(continueOnCapturedContext: false);
+
+            using var server = await tcpListener.AcceptTcpClientAsync();
+            using var stream = new StreamReader(server.GetStream());
+
+            Assert.Equal(passMessage, await stream.ReadLineAsync());
+        }
+    }
+}
