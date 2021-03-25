08e73d83e8b8a21a0b269a407de7bd3813cb42f8
Fredi Machado <fredisoft@gmail.com>
2021-03-25 18:48:52 +1100

    Add Kick message and handler

---
+++ src/NetIRC/Messages/Handlers/KickHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NetIRC.Messages.Handlers
+{
+    public class KickHandler : MessageHandler<KickMessage>
+    {
+        public override Task HandleAsync(KickMessage serverMessage, Client client)
+        {
+            var channel = client.Channels.GetChannel(serverMessage.Channel);
+            if (channel != null)
+            {
+                if (string.Equals(serverMessage.Nick, client.User.Nick, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    client.Channels.Remove(channel);
+                }
+                else
+                {
+                    channel.RemoveUser(serverMessage.Nick);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/NetIRC/Messages/KickMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace NetIRC.Messages
+{
+    public class KickMessage : IRCMessage, IServerMessage
+    {
+        public string KickedBy { get; }
+        public string Channel { get; }
+        public string Nick { get; }
+        public string Comment { get; set; }
+
+        public KickMessage(ParsedIRCMessage parsedMessage)
+        {
+            KickedBy = parsedMessage.Prefix.From;
+            Channel = parsedMessage.Parameters[0];
+            Nick = parsedMessage.Parameters[1];
+            Comment = parsedMessage.Trailing;
+        }
+    }
+}
